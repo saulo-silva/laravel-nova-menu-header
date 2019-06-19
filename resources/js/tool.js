f637f5830ff400fb2c70bc3abd1ed09d9fec95b6
Saulo Silva <saulo@s10design.com.br>
2019-06-19 18:25:40 -0300

    menu header

---
+++ resources/js/tool.js
@@ -0,0 +1,9 @@
+Nova.booting((Vue, router, store) => {
+    // router.addRoutes([
+    //     {
+    //         name: 'menu-header',
+    //         path: '/menu-header',
+    //         component: require('./components/Tool'),
+    //     },
+    // ])
+})
